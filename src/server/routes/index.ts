a4492c2574fb31b5995657d5b6c49b015aed6643
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-01-16 21:58:52 -0300

    feat: create routes

---
+++ src/server/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from "express"
+import { StatusCodes } from 'http-status-codes'
+
+const router = Router()
+
+
+router.get('/', (req, res) => {
+  res.send('Olá, Dev')
+})
+
+router.post('/test', (req, res) => {
+  console.log(req.body)
+  return res.status(StatusCodes.ACCEPTED).json(req.body)
+})
+
+export { router }
