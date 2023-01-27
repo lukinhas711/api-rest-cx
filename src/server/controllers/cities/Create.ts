05ecd9d9ade8df69e96d0ba15fc90da6fa19d1ac
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-01-27 00:11:50 -0300

    yup validation

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,14 +1,30 @@
 import { Request, Response } from 'express'
+import consola from 'consola'
+import { StatusCodes } from 'http-status-codes'
+import * as yup from 'yup'
 
 interface iCity {
-  nome: string
+  name: string
 }
 
-export const create = (req: Request<{}, {}, iCity>, res: Response) => {
-  const data = req.body
+const bodyValidation: yup.SchemaOf<iCity> = yup.object().shape({
+  name: yup.string().required().min(3)
+})
 
-  console.log(data)
+export const create = async (req: Request<{}, {}, iCity>, res: Response) => {
+  let validatedData: iCity | undefined = undefined
 
+  try {
+    validatedData = await bodyValidation.validate(req.body)
+    consola.success(validatedData)
+  } catch (error) {
+    const yupError = error as yup.ValidationError
+    res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: yupError.message
+      }
+    })
+  }
 
   return res.send('Create cities controller')
 }
